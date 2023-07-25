5ec279afa32b3334de9dee8c83fa1b7c91cb63c7
Gaurav Shrimal <gaurav.shrimal@ibm.com>
2023-07-25 19:53:58 +0530

    added hw v2

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -15,7 +15,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World v2";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
